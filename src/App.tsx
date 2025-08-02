f89ba06347c07b810e51f3c861330965f7c186eb
Hossein Amer <hossein.esmat@gmail.com>
2025-08-02 20:15:26 +0200

    Final touches

--- src/App.tsx
+++ src/App.tsx
@@ -38,7 +38,7 @@ function App() {
     {
       name: "Traditionell Rakning",
       description: "Klassisk rakning med rakkniv och varmt handduk",
-      price: "Från 300 kr",
+      price: "",
       image: "https://images.pexels.com/photos/1570806/pexels-photo-1570806.jpeg?auto=compress&cs=tinysrgb&w=400"
     },
     {
